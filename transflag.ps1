fe470c9afc3ddd47f0d6c0345cc9b17890a5a232
Herremil <Gamingemil09@gmail.com>
2025-04-14 15:57:15 +0200

    Create transflag.ps1

---
+++ transflag.ps1
@@ -0,0 +1,5 @@
+Write-Host "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" -ForegroundColor Cyan
+Write-Host "=====================================" -ForegroundColor Magenta
+Write-Host ":::::::::::::::::::::::::::::::::::::" -ForegroundColor White
+Write-Host "=====================================" -ForegroundColor Magenta
+Write-Host "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" -ForegroundColor Cyan
